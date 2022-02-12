07b1e74ff839496c33b6940484973ea5f95f91b3
Karim <abdelkarim.kh95@gmail.com>
2022-02-12 22:51:33 +0200

    block user api

--- php/add_request.php
+++ php/add_request.php
@@ -3,19 +3,32 @@ session_start();
 
 include("db_info.php");
 
-$id1 = $_SESSION["id"];
+// $id1 = $_SESSION["id"];
+$id1 = 1;
 $id2 = $_GET["id2"];
 $is_pending = 1;
 
-$query = $mysqli->prepare("INSERT INTO connections (user1_id, user2_id, is_pending) VALUES (?,?,?)"); 
-$query->bind_param("ssi", $id1, $id2, $is_pending);
-$query->execute();
+$check_query = $mysqli->prepare("SELECT * FROM connections where user1_id=? AND user2_id=?");
+$check_query->bind_param("ss", $id1,$id2);
+$check_query->execute();
+$check_query->store_result();
+$num_rows = $check_query->num_rows;
+$check_query->fetch();
 
 $array_response = [];
-$array_response["status"] = "request sent";
+
+if ($num_rows == 0){
+    $query = $mysqli->prepare("INSERT INTO connections (user1_id, user2_id, is_pending) VALUES (?,?,?)"); 
+    $query->bind_param("ssi", $id1, $id2, $is_pending);
+    $query->execute();
+    $array_response["status"] = "request sent";
+}else{
+    $array_response["status"] = "connection already exists";
+}
 
 $json_response = json_encode($array_response);
 echo $json_response;
+
 
 
 ?>
