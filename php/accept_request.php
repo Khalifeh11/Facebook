f65540d3d37e4bf0ab5eb88cd59a7771d1904d59
Karim <abdelkarim.kh95@gmail.com>
2022-02-12 21:35:51 +0200

    delete reuqest API

--- php/accept_request.php
+++ php/accept_request.php
@@ -3,12 +3,12 @@ session_start();
 
 include("db_info.php");
 
-$id1 = $_SESSION["id"];
-$id2 = $_GET["id2"];
+$id1 = $_GET["id2"];
+$id2 = $_SESSION["id"];
 $is_pending = 0;
 
-$query = $mysqli->prepare("UPDATE connections SET is_pending = ? WHERE user_id1=? AND user_id2=?"); 
-$query->bind_param("ssi", $is_pending, $id1, $id2);
+$query = $mysqli->prepare("UPDATE connections SET is_pending = ? WHERE user1_id=? AND user2_id=?"); 
+$query->bind_param("iss", $is_pending, $id1, $id2);
 $query->execute();
 
 $array_response = [];
