eac7330315b7390031e5073ff6d93d0628e84a30
Karim <abdelkarim.kh95@gmail.com>
2022-02-14 13:27:02 +0200

    sign up js validation

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,11 @@ signinBtn.addEventListener("click", function (e) {
   const email = emailField.value;
   const password = passwordField.value;
   if (email.length ==0){
-      emailField.style.border = "solid #FF0000";
+      emailField.style.border = "1px solid #FF0000";
   }
 
   if (password.length ==0){
-      passwordField.style.border = "solid #FF0000";
+      passwordField.style.border = "1px solid #FF0000";
  }
 
   fetchLogin(email, password)
@@ -38,7 +38,7 @@ async function fetchLogin(email, password) {
       localStorage.setItem("user_id", json.user_id);
       localStorage.setItem("first_name", json.first_name);
       localStorage.setItem("last_name", json.last_name);
-      location.href = "../views/home.html";
+      location.href = " ./views/home.html";
     }
   } catch (error) {
     console.log("error", error);
@@ -47,12 +47,30 @@ async function fetchLogin(email, password) {
 
 
 const registerBtn = document.getElementById("signup-btn");
+const registerEmail = document.getElementById("register-email");
+const registerPassword = document.getElementById("register-password");
+const registerFirstName = document.getElementById("register-first-name");
+const registerLastName = document.getElementById("register-last-name");
+
+
   registerBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  const email = document.getElementById("register-email").value;
-  const firstName = document.getElementById("register-first-name").value;
-  const lastName = document.getElementById("register-last-name").value;
-  const password = document.getElementById("register-password").value;
+  const email = registerEmail.value;
+  const firstName = registerFirstName.value;
+  const lastName = registerLastName.value;
+  const password = registerPassword.value;
+  if (email.length ==0){
+    registerEmail.style.border = "1px solid #FF0000";
+}
+if (password.length ==0){
+  registerPassword.style.border = "1px solid #FF0000";
+}
+if (firstName.length ==0){
+  registerFirstName.style.border = "1px solid #FF0000";
+}
+if (lastName.length==0){
+  registerLastName.style.border ="1px solid #FF0000";
+}
   fetchRegister(firstName, lastName, email, password);
 });
 
