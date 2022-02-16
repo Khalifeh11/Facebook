ccf98b973e0c1ed4a658a764741db505415d79ca
Karim <abdelkarim.kh95@gmail.com>
2022-02-17 01:25:38 +0200

    accept request api and fix get requests api

--- php/accept_request.php
+++ php/accept_request.php
@@ -3,12 +3,13 @@ session_start();
 
 include("db_info.php");
 
-$id1 = $_GET["id2"];
-$id2 = $_SESSION["id"];
+$id1 = $_GET["id1"];
+$id1 = base64_decode($id1);
+$id2 = $_GET["id2"];
 $is_pending = 0;
 
-$query = $mysqli->prepare("UPDATE connections SET is_pending = ? WHERE user1_id=? AND user2_id=?"); 
-$query->bind_param("iss", $is_pending, $id1, $id2);
+$query = $mysqli->prepare("UPDATE connections SET is_pending = ? WHERE (user1_id=? AND user2_id=?) OR (User1_id=? AND user2_id=?)"); 
+$query->bind_param("sssss", $is_pending, $id1, $id2, $id2, $id1);
 $query->execute();
 
 $array_response = [];
