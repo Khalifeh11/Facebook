c07b80cb0c78d9c050a56049f950a53b4d690a0c
Karim <abdelkarim.kh95@gmail.com>
2022-02-16 19:02:48 +0200

    get friends api fix queries

--- php/get_friends.php
+++ php/get_friends.php
@@ -2,24 +2,38 @@
 
 include("db_info.php");
 
-// $id = $_SESSION["id"];
 $id = $_GET["id"];
 $id = base64_decode($id);
 $is_blocked = 0;
 $is_pending = 0;
 
-$query = $mysqli->prepare("SELECT user1_id, user2_id FROM connections where (user1_id=? or user2_id =?) AND is_blocked = ? AND is_pending = ?");
+$query = $mysqli->prepare("SELECT user1_id FROM connections where (user2_id=?) AND is_blocked = ? AND is_pending = ?");
 
-$query->bind_param("iiii", $id, $id, $is_blocked, $is_pending);
+
+$query->bind_param("iii", $id, $is_blocked, $is_pending);
 $query->execute();
 
-$result = $query->get_result();
-$friends =[];
-while($result_array = $result->fetch_assoc()){
-    $friends[] = $result_array;
+$result1 = $query->get_result();
+$friends1 =[];
+while($result1_array = $result1->fetch_assoc()){
+    $friends1[] = $result1_array;
 }
+
+$query2 = $mysqli->prepare("SELECT user2_id FROM connections where (user1_id=?) AND is_blocked = ? AND is_pending = ?");
+
+$query2->bind_param("iii", $id, $is_blocked, $is_pending);
+$query2->execute();
+
+$result2 = $query2->get_result();
+$friends2 =[];
+while($result2_array = $result2->fetch_assoc()){
+    $friends2[] = $result2_array;
+}
+
+$friends = array_merge($friends1, $friends2);   
 
 $json = json_encode($friends, JSON_PRETTY_PRINT);
 echo($json);
 
 ?>
+
--- php/get_posts.php
+++ php/get_posts.php
@@ -6,7 +6,7 @@ session_start();
 include("db_info.php");
 
 $id = $_POST["id"];
-// $id = base64_decode($id);
+$id = base64_decode($id);
 
 
 // $query = $mysqli->prepare("SELECT DISTINCT(p.ID), p.Post_content, p.nb_of_likes, p.nb_of_dislikes, p.User_ID, p.post_time FROM posts p, connections c WHERE (p.User_ID = c.user1_id OR p.User_ID = c.user2_id) AND is_blocked = 0 AND is_pending = 0 AND (user1_id = ? OR user2_id = ?) ORDER BY post_time desc");
