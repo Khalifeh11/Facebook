c8d56b0cac6342bde7b8ea17a8c20ff107301d23
Karim <abdelkarim.kh95@gmail.com>
2022-02-17 00:28:56 +0200

    get_user_posts api

--- php/edit_post.php
+++ php/edit_post.php
@@ -3,14 +3,14 @@ session_start();
 
 include("db_info.php");
 
-// $id = $_SESSION["id"];
-$id = 1;
-$text = $_GET["text"];
-$post_id = $_GET["post_id"];
+
+$text = $_POST["text"];
+$post_id = $_POST["post_id"];
 
 
-$query = $mysqli->prepare("UPDATE posts SET Post_content=? WHERE User_ID=? AND ID=?");
-$query->bind_param("sss", $text, $id, $post_id);
+$query = $mysqli->prepare("UPDATE posts SET Post_content=? WHERE ID=?");
+
+$query->bind_param("ss", $text, $post_id);
 $query->execute();
 
 $array_response = [];
--- php/get_posts.php
+++ php/get_posts.php
@@ -1,15 +1,8 @@
 <?php
-
-
-session_start();
-
 include("db_info.php");
 
 $id = $_POST["id"];
 $id = base64_decode($id);
-
-
-// $query = $mysqli->prepare("SELECT DISTINCT(p.ID), p.Post_content, p.nb_of_likes, p.nb_of_dislikes, p.User_ID, p.post_time FROM posts p, connections c WHERE (p.User_ID = c.user1_id OR p.User_ID = c.user2_id) AND is_blocked = 0 AND is_pending = 0 AND (user1_id = ? OR user2_id = ?) ORDER BY post_time desc");
 
 $query = $mysqli->prepare("SELECT * from posts WHERE User_ID =? 
 or (
@@ -19,7 +12,6 @@ or (
     User_ID IN (SELECT user2_id from connections WHERE (user2_id=? or user1_id=?) and is_blocked <> 1 and is_pending <> 1)
     ) ORDER BY post_time desc");
 
-// 2 3 4 22
 $query->bind_param("sssss", $id,$id,$id,$id,$id);
 $query->execute();
 
@@ -32,7 +24,6 @@ while($connection1 = $result->fetch_assoc()){
 }
 
 
-// $array_response["posts"] = $connections;
 $json_response = json_encode($connections);
 echo $json_response;
 
